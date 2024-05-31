5e210744f1853e94d365895b945e1dcf4827c998
Yuliya Barshcheuskaya <01194350@pw.edu.pl>
2024-05-31 14:33:05 +0200

    changed to create empty vector

--- main.cpp
+++ main.cpp
@@ -6,11 +6,10 @@
 
 int main()
 {
-    Snake s1("RED",'a','d');
     std::vector<Snake> Players;
-    Players.push_back(s1);
     Board board(Players);
     board.display_window();
 
     return 0;
 }
+
