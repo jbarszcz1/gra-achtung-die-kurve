ca8905ebbc4fe09350f8616a01e06be0d112c6f9
Yuliya Barshcheuskaya <01194350@pw.edu.pl>
2024-06-03 12:32:56 +0200

    added final documentation

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/jakub/PROI/projekt/gra_achtung_die_kurve
-# Build directory: C:/Users/jakub/PROI/projekt/gra_achtung_die_kurve/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/raylib-build")
-subdirs("_deps/googletest-build")
